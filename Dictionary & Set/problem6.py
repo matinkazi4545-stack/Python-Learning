81657dc7420641030e6cfcf22eeb8c70d8c3d3a6
Matin Kazi <matinkazi4545@gmail.com>
2026-08-19 00:35:18 +0530

    Add files via upload

---
+++ Dictionary & Set/problem6.py
@@ -0,0 +1,16 @@
+d = {}
+
+name = input("Enter friends name: ")
+lang = input("Enter Language name: ")
+d[lang] = name
+
+name = input("Enter friends name: ")
+lang = input("Enter Language name: ")
+d[lang] = name
+
+name = input("Enter friends name: ")
+lang = input("Enter Language name: ")
+d[lang] = name
+
+
+print(d)
---
+++ Dictionary & Set/problem9.py
@@ -0,0 +1,17 @@
+s = [8, 7, 12, "Harry", [1,2]]
+
+print (type(s))
+
+s = (8, 7, 12, "Harry", [1,2])
+
+print (type(s))
+
+s = {8, 
+     7, 
+     12, "Harry",}
+
+print (type(s))
+
+s = "8, 7, 12, harry "
+
+print (type(s)) 
